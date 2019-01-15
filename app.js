7d828c604dbd3fcc86c6c5e63ef2008fac9eba78
luzlopez <calu.lopez@gmail.com>
2019-01-14 21:02:52 -0300

    cosas

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 
 $(document).ready(function(){
     var url = 'https://mindicador.cl/api';
+    var uf = 'uf';
+    var dolar = 'dlr';
+    var utm = 'utm';
+    var euro = 'eu';
+
 	 
+	$('#select').on('change', function () {
+		$.ajax ({
+		url:  url + key + '/' + coords ['scl'],
+		method:'GET'
+	}).then(function (data) {
+			   console.log(data);
+		});
+	  });
+
+
+
+ 
