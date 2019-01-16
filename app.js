22643338b11be34977d7329064ea06927b7ae483
luzlopez <calu.lopez@gmail.com>
2019-01-15 21:17:41 -0300

    archivos modificados

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 
 $(document).ready(function(){
-    var url = 'https://mindicador.cl/api';
-    var uf = 'uf';
-    var dolar = 'dlr';
-    var utm = 'utm';
-    var euro = 'eu';
+	
+    var uf = '(#UF)';
+    var dolar = '(#Dolar)';
+    var utm = '(UTM)';
+    var euro = '(Euro)';
+    var divisa = '(Divisa)';
 
 	 
-	$('#select').on('change', function () {
-		$.ajax ({
-		url:  url + key + '/' + coords ['scl'],
+	$.ajax ({
+		url:  'https://mindicador.cl/api',
 		method:'GET'
 	}).then(function (data) {
-			   console.log(data);
+			 console.log(data);
+			 $('#Dolar').text(data.dolar.valor);
+			 $('#Euro').text(data.euro.valor);
+			 $('#UF').text(data.uf.valor);
+			 $('#UTM').text(data.utm.valor);
+			
+
 		});
-	  });
+})
 
 
-
- 
+//onclick boton enviar
+// $val del placeholder 1 clp x ejemplo, se declara como variable para poder aplicar las operaciones matematicas
--- borrador.js
+++ borrador.js
@@ -1,1 +1,38 @@
-borrador.js
+$(document).ready(function(){
+    $(function (){
+    	$.ajax ({
+    		method: 'GET'
+    		url: 'https:/mindicador.cl/api'
+
+
+
+    	})
+
+    	.then (function (data) {
+    		rates %data
+    		$('.indicador.usd').html(formatNumber(rates.dolar.valor));
+    		$('.indicador.euro').html(formatNumber(rates.euro.valor));
+    		$('.indicador.utm').html(formatNumber(rates.utm.valor));
+    		$('.indicador.uf').html(formatNumber(rates.uf.valor));
+
+    	});
+
+
+    	$('#exchange-form input') .keyup(function(e)){
+    		e. preventDefault();
+    		var amount = $('.clp_input').val();
+    		$(.'exchange-result.usd').html(formatNumber(rates.dolar.valor * amount));
+    		$(.'exchange-result.euro').html(formatNumber(rates.euro.valor * amount));
+    		$(.'exchange-result.utm').html(formatNumber(rates.utm.valor * amount));
+    		$(.'exchange-result.uf').html(formatNumber(rates.uf.valor * amount));
+
+
+    	}
+
+
+
+
+    	}
+
+
+    }
